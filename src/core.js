9a51d09fc5380415eb1462c37d5c5a1851ff671c
Marcin Warpechowski <warpech@gmail.com>
2012-11-16 00:13:47 +0100

    courtains initial commit

--- src/core.js
+++ src/core.js
@@ -31,4 +31,49 @@ function Walkontable(TABLE) {
       that.currentSelection.add(TD);
     }
   });
+
+  function removeTextNodes(elem, parent) {
+    if (elem.nodeType === 3) {
+      parent.removeChild(elem); //bye text nodes!
+    }
+    else if (['TABLE', 'THEAD', 'TBODY', 'TFOOT', 'TR'].indexOf(elem.nodeName) > -1) {
+      var childs = elem.childNodes;
+      for (var i = childs.length - 1; i >= 0; i--) {
+        removeTextNodes(childs[i], elem);
+      }
+    }
+  }
+
+  removeTextNodes(TABLE);
+
+  var rightBox = [];
+
+  var BUTTON = document.getElementById('detach');
+  this.wtDom.addEvent(BUTTON, 'click', function () {
+    var underbed = [];
+    var TSECTIONs = TABLE.childNodes;
+    for (var i = 0, ilen = TSECTIONs.length; i < ilen; i++) {
+      underbed[i] = [];
+      var TRs = TSECTIONs[i].childNodes;
+      for (var j = 0, jlen = TRs.length; j < jlen; j++) {
+        var TDs = TRs[j].childNodes;
+        var TD = TRs[j].removeChild(TDs[TDs.length - 1]);
+        underbed[i].push(TD);
+      }
+    }
+    rightBox.push(underbed);
+  });
+
+  BUTTON = document.getElementById('attach');
+  this.wtDom.addEvent(BUTTON, 'click', function () {
+    var underbed = rightBox.pop();
+    if(underbed) {
+      console.log("idzie", underbed);
+      for (var i = 0, ilen = underbed.length; i < ilen; i++) {
+        for (var j = 0, jlen = underbed[i].length; j < jlen; j++) {
+          TABLE.childNodes[i].childNodes[j].appendChild(underbed[i][j]);
+        }
+      }
+    }
+  });
 }
