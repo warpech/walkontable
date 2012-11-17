5fb6c96871c73b6e91b9586a2a2124edda4f3227
Marcin Warpechowski <warpech@gmail.com>
2012-11-17 18:03:11 +0100

    WalkontableTable initial release

---
+++ src/table.js
@@ -0,0 +1,26 @@
+function WalkontableTable(TABLE) {
+  this.TABLE = TABLE;
+  this.wtCell = new WalkontableCell();
+}
+
+WalkontableTable.prototype.getColumn = function (index) {
+  var column = []
+    , TD
+    , colIndex
+    , s
+    , r
+    , c;
+  for (s = 0, slen = this.TABLE.childNodes.length; s < slen; s++) {
+    for (r = 0, rlen = this.TABLE.childNodes[s].childNodes.length; r < rlen; r++) {
+      for (c = 0, clen = this.TABLE.childNodes[s].childNodes[r].childNodes.length; c < clen; c++) {
+        TD = this.TABLE.childNodes[s].childNodes[r].childNodes[c];
+        colIndex = this.wtCell.colIndex(TD);
+        if (colIndex === index || colIndex + TD.colSpan > index) {
+          column.push(TD);
+          break;
+        }
+      }
+    }
+  }
+  return column;
+};
---
+++ test/jasmine/spec/table.spec.js
@@ -0,0 +1,42 @@
+describe('WalkontableTable', function () {
+  var $table;
+
+  var debug = false;
+
+  beforeEach(function () {
+    $table = $('<table border=1><tr><td></td><td></td><td rowspan="2"></td><td></td></tr><tr><td colspan="2"></td><td></td></tr><tr><td></td><td></td><td></td><td></td></tr><tr><td colspan="2"></td><td></td><td></td></tr></table>'); //create a table that is not even attached to document
+    if (debug) {
+      $table.find('td').each(function () {
+        this.innerHTML = 'x';
+      });
+    }
+  });
+
+  afterEach(function () {
+    if (debug) {
+      $table.appendTo('body');
+    }
+  });
+
+  /**
+   * getColumn
+   */
+
+  it("should get first column", function () {
+    var wtTable = new WalkontableTable($table[0]);
+    var TDs = wtTable.getColumn(0);
+    expect(TDs[0]).toBe($table.find('tr:eq(0) td:eq(0)')[0]);
+    expect(TDs[1]).toBe($table.find('tr:eq(1) td:eq(0)')[0]);
+    expect(TDs[2]).toBe($table.find('tr:eq(2) td:eq(0)')[0]);
+    expect(TDs[3]).toBe($table.find('tr:eq(3) td:eq(0)')[0]);
+  });
+
+  it("should get last column", function () {
+    var wtTable = new WalkontableTable($table[0]);
+    var TDs = wtTable.getColumn(3);
+    expect(TDs[0]).toBe($table.find('tr:eq(0) td:eq(3)')[0]);
+    expect(TDs[1]).toBe($table.find('tr:eq(1) td:eq(1)')[0]);
+    expect(TDs[2]).toBe($table.find('tr:eq(2) td:eq(3)')[0]);
+    expect(TDs[3]).toBe($table.find('tr:eq(3) td:eq(2)')[0]);
+  });
+});
