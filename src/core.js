d1470981daeba86fc21a2cd27ff24cc226aae891
Marcin Warpechowski <warpech@gmail.com>
2012-11-22 01:19:19 +0100

    scroll on mouse wheel

--- src/core.js
+++ src/core.js
@@ -7,6 +7,7 @@ function Walkontable(settings) {
   //bootstrap from settings
   this.wtTable = new WalkontableTable(this);
   this.wtScroll = new WalkontableScroll(this);
+  this.wtWheel = new WalkontableWheel(this);
   this.wtEvent = new WalkontableEvent(this);
   this.wtDom = new WalkontableDom();
   this.displayColumns = this.wtTable.THEAD.childNodes[0].childNodes.length;
@@ -41,6 +42,18 @@ Walkontable.prototype.update = function (settings) {
     if (settings.hasOwnProperty(i)) {
       this.settings[i] = settings[i];
     }
+  }
+  return this;
+};
+
+Walkontable.prototype.scrollVertical = function (delta) {
+  var max = this.settings.data.length - 1 - this.settings.displayRows;
+  this.settings.startRow = this.settings.startRow + delta;
+  if (this.settings.startRow < 0) {
+    this.settings.startRow = 0;
+  }
+  else if (this.settings.startRow >= max) {
+    this.settings.startRow = max;
   }
   return this;
 };
---
+++ src/wheel.js
@@ -0,0 +1,15 @@
+function WalkontableWheel(instance) {
+  var that = this;
+
+  //reference to instance
+  this.instance = instance;
+  $(this.instance.settings.table).on('mousewheel', function (event, delta, deltaX, deltaY) {
+    if (deltaY) {
+      that.instance.scrollVertical(-deltaY).draw();
+    }
+    else if (deltaX) {
+
+    }
+    event.preventDefault();
+  });
+}
