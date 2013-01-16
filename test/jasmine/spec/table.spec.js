16b0091136f6aad6e224d5505c55f32809d963c6
Marcin Warpechowski <warpech@gmail.com>
2013-01-16 21:33:33 +0100

    remove duplicated test

--- test/jasmine/spec/table.spec.js
+++ test/jasmine/spec/table.spec.js
@@ -570,26 +570,6 @@ describe('WalkontableTable', function () {
     expect($table.find('tbody tr:first td:eq(2)').html()).toBe('');
   });
 
-  it("should not render a cell that is outside of the viewport horizontally", function () {
-    var wt = new Walkontable({
-      table: $table[0],
-      data: getData,
-      totalRows: getTotalRows,
-      totalColumns: getTotalColumns,
-      width: 200,
-      height: 100,
-      offsetRow: 0,
-      offsetColumn: 0,
-      columnWidth: 100
-    });
-    wt.draw();
-    $table.find('tbody td').html('');
-    wt.draw();
-    expect($table.find('tbody tr:first td').length).toBeGreaterThan(3); //there are more columns though there won't be rendered
-    expect($table.find('tbody tr:first td:eq(1)').html()).not.toBe('');
-    expect($table.find('tbody tr:first td:eq(2)').html()).toBe('');
-  });
-
   it("should not render a cell when selectionsOnly == true", function () {
     var count = 0
       , wt = new Walkontable({
